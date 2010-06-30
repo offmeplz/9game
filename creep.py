e7e57aab91801dceeb389c67955c520ee493252e
Dmitry A. Ermolov <epdmitry@yandex.ru>
2010-07-01 01:16:05 +0400

    Creep moves through centers

--- creep.py
+++ creep.py
@@ -19,6 +19,8 @@ class Creep(pygame.sprite.Sprite):
         self.field = field
         self.image, self.rect = self.get_img_rect()
         self.rect.center = util.game2cscreen(g_pos)
+        self.curdst = None
+        self.cursrc = None
 
     @classmethod
     def get_img_rect(cls):
@@ -29,15 +31,22 @@ class Creep(pygame.sprite.Sprite):
     def update(self, ticks):
         g_pos = Vec(int(self.g_pos[0]), int(self.g_pos[1]))
         cur_cell = self.field._get_cell(g_pos)
-        if cur_cell.is_exit:
-            self.finish()
-        else:
-            g_next_pos = self.field.get_next_pos(g_pos)
-            g_next_pos = Vec(g_next_pos)
-            g_dpos = g_next_pos - g_pos
-            g_dpos *= ticks * self.speed / float(TICK_PER_SEC)
-            self.g_pos += g_dpos
-            self.rect.center = util.game2cscreen(self.g_pos)
+        if self.curdst is None:
+            if cur_cell.is_exit:
+                self.finish()
+                return
+            dst = self.field.get_next_pos(g_pos)
+            self.curdst = Vec(dst)
+            self.cursrc = Vec(g_pos)
+        dstvec = self.curdst - self.g_pos
+        g_dpos = self.curdst - self.cursrc
+        g_dpos /= abs(g_dpos)
+        g_dpos *= ticks * self.speed / float(TICK_PER_SEC)
+        if abs(g_dpos) >= abs(dstvec):
+            g_dpos = dstvec
+            self.curdst = None
+        self.g_pos += g_dpos
+        self.rect.center = util.game2cscreen(self.g_pos)
 
     def finish(self):
         pass
