4081b76558946efc3dbe91562c56c4682a2ae7d6
Dmitry A. Ermolov <epdmitry@yandex.ru>
2010-07-06 21:22:52 +0400

    Changed field size, added panel size.

--- cfg.py
+++ cfg.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 #vim:fileencoding=utf-8
 
-GAME_X_SIZE = 7
-GAME_Y_SIZE = 11
+GAME_X_SIZE = 13
+GAME_Y_SIZE = 13
 GAME_CELL_SIZE = 32
+
+PANEL_X_SIZE = 100
 
 TICK_PER_SEC = 30
 
