3a10a9302e67aeb35ba6ca3aa5ebcfc92eefd8f6
Dmitry A. Ermolov <epdmitry@yandex.ru>
2010-07-06 21:41:57 +0400

    Some comments.

--- cfg.py
+++ cfg.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 #vim:fileencoding=utf-8
 
-GAME_X_SIZE = 13
+# Field sizes (in game cells).
+GAME_X_SIZE = 13 
 GAME_Y_SIZE = 13
+
+# Size in pixels of one game cell.
 GAME_CELL_SIZE = 32
 
+# Panel width.
 PANEL_X_SIZE = 100
 
 TICK_PER_SEC = 30
