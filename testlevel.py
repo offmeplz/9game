acc1bf927ee91cbc6ee6fabe150553b432196b89
Dmitry A. Ermolov <epdmitry@yandex.ru>
2010-07-12 11:18:31 +0400

    Tower balance example, some comments.

--- testlevel.py
+++ testlevel.py
@@ -4,7 +4,7 @@
 from itertools import count
 from gameobjects import Creep, Wall, SimpleTower, LightningTower
 
-# Set map
+# Set map (Unsupported for now)
 # > -- enter
 # ^ -- exit
 # X -- obstacle
@@ -29,9 +29,29 @@ BACKGROUND = None
 
 # Waves of creeps.
 CREEP_WAVES = [
-        (0, dict(creeps=((i * 2, Creep(health=5, speed=2)) for i in count()))),
+        # (StartTime, WaveDescription)
+
+        # 10 creeps, spawn each 2 seconds.
+        (0, dict(creeps=((i * 2, Creep(health=5, speed=2)) for i in xrange(10)))),
+        # infinite number of creeps, spawn each second.
         (20, dict(creeps=((i, Creep(health=10, speed=1.5, money=2)) for i in count()))),
         ]
+
+
+# Balance tower parameters
+Wall.cost = 2
+
+SimpleTower.cost = 5
+SimpleTower.damage = 1
+SimpleTower.radius = 4
+SimpleTower.recharge_time = 2
+SimpleTower.bullet_speed = 5
+
+LightningTower.cost = 10
+LightningTower.damage = 3
+LightningTower.radius = 3
+LightningTower.recharge_time = 3
+LightningTower.fire_cost = 1
 
 # Availible towers.
 TOWERS = [
